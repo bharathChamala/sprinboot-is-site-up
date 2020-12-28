84a85401271b36c27fcc58959f6780d601d3adba
Bharath Chamala <bharathreddycv26@gmail.com>
2020-12-27 17:13:54 -0700

    initial commit

---
+++ src/main/java/com/bharathinc/testissiteup/TestIsSiteUpApplication.java
@@ -0,0 +1,14 @@
+package com.bharathinc.testissiteup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIsSiteUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIsSiteUpApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
