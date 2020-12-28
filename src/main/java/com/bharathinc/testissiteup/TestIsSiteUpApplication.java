39ae33cf1096d8a2d9b7899e6bcd8bed82139668
Bharath Chamala <bharathreddycv26@gmail.com>
2020-12-27 17:22:47 -0700

    Test commit from vsCode

--- src/main/java/com/bharathinc/testissiteup/TestIsSiteUpApplication.java
+++ src/main/java/com/bharathinc/testissiteup/TestIsSiteUpApplication.java
@@ -9,6 +9,7 @@ public class TestIsSiteUpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestIsSiteUpApplication.class, args);
 		System.out.println("Hello world");
+		//git commit
 	}
 
 }
